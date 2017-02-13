964c854eadb23ff4a122182f4aa012d0695353b9
adonofrio <ales.donofrio@gmail.com>
2017-02-13 22:43:31 +0100

    The Tennis class has been implemented.

--- TennisGame/Tennis.cs
+++ TennisGame/Tennis.cs
@@ -8,39 +8,119 @@ namespace TennisGame
 {
     public class Tennis
     {
+        private int player1Points = 0;
+        private int player2Points = 0;
+
         public PlayerEnum? GetTheWinner()
         {
-            throw new NotImplementedException();
+            PlayerEnum? winner = null;
+            if (player1Points >= player2Points + 2)
+            {
+                if (player1Points >= 4)
+                {
+                    winner = PlayerEnum.Player1;
+                }
+            }
+            else if (player2Points >= player1Points + 2)
+            {
+                if (player2Points >= 4)
+                {
+                    winner = PlayerEnum.Player2;
+                }
+            }
+            return winner;
         }
 
         public void SetGainedPoint(PlayerEnum player)
         {
-            throw new NotImplementedException();
+            if (!HasGameEnded())
+            {
+                if (player == PlayerEnum.Player1)
+                {
+                    player1Points++;
+                }
+                else
+                {
+                    player2Points++;
+                }
+            }
         }
 
-        public int GetNumScore(PlayerEnum player1)
+        public int GetNumScore(PlayerEnum player)
         {
-            throw new NotImplementedException();
+            int retValue = 0;
+            if (player == PlayerEnum.Player1)
+            {
+                retValue = player1Points;
+            }
+            else
+            {
+                retValue = player2Points;
+            }
+            return retValue;
         }
 
         public void StartNewGame()
         {
-            throw new NotImplementedException();
+            player1Points = 0;
+            player2Points = 0;
         }
 
         public bool HasGameEnded()
         {
-            throw new NotImplementedException();
+            return GetTheWinner().HasValue;
         }
 
-        public string GetDescScore(PlayerEnum player1)
+        public string GetDescScore(PlayerEnum player)
         {
-            throw new NotImplementedException();
+            string score;
+            int pointToAnalyze;
+            if (player == PlayerEnum.Player1)
+            {
+                pointToAnalyze = player1Points;
+            }
+            else
+            {
+                pointToAnalyze = player2Points;
+            }
+            if (pointToAnalyze > 3)
+            {
+                score = pointToAnalyze.ToString();
+            }
+            else if (pointToAnalyze >= 0)
+            {
+                score = Enum.GetNames(typeof(ScoreEnum))[pointToAnalyze];
+            }
+            else
+            {
+                score = string.Empty;
+            }
+            return score;
         }
 
         public string GetGameScore()
         {
-            throw new NotImplementedException();
+            string gameScore = string.Empty;
+            if (player1Points == player2Points && player1Points >= 3)
+            {
+                gameScore = GameUtility.Deuce;
+            }
+            else if (player1Points >= 3 && player2Points >= 3)
+            {
+                if (player1Points == player2Points + 1)
+                {
+                    gameScore = GameUtility.GetAdvantageMessage(PlayerEnum.Player1);
+                }
+                else if (player2Points == player1Points + 1)
+                {
+                    gameScore = GameUtility.GetAdvantageMessage(PlayerEnum.Player2);
+                }
+            }
+            else
+            {
+                gameScore = string.Format("{0} - {1}", GetDescScore(PlayerEnum.Player1), GetDescScore(PlayerEnum.Player2));
+            }
+            return gameScore;
         }
     }
 }
